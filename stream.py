8112dde99ae66ce247eca84de24f2987ce0522a6
rangapv <rangapv@yahoo.com>
2021-09-18 13:18:06 +0000

    Building a website in python

---
+++ stream.py
@@ -0,0 +1,15 @@
+# import module
+import streamlit as st
+# Title
+st.title("This is Title line")
+# Header
+st.header("This is a header")
+# Subheader
+st.subheader("This line belongs to a subheader")
+# Text
+st.text("This line belongs to a text")
+# Markdowns
+st.markdown("### This is a markdown")
+st.markdown("## This is a markdown")
+st.markdown("# This is a markdown")
+
